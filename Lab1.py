fc45a93e99831e4f8f98dd747eb2f4eef0c03cfa
ShevchenkoVl <7valentin7777@gmail.com>
2021-10-23 09:25:55 +0300

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,11 @@
+#3 курс 3 лаб
+from math import *
+print("3 курс, 3 лаб")
+print("Валентин Шевченко, І-219а")
+x = float(input("input x "))
+b = float(input("input b "))
+if(b==0):
+	print("Error")
+else:
+	y = sqrt(x-b)/(2*b)-tan(x)/pow(b,2)
+	print(yb)
